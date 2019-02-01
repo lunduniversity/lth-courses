467cdfd22c6153c5ce6da5ed4cf4aeed0b62390c
bjornregnell <bjorn.regnell@cs.lth.se>
2019-02-01 20:06:46 +0100

    eat server params

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,13 @@
 object Main {
   def main(args: Array[String]): Unit = args.toList match {
     case "--download" :: xs => DownloadAllCoursePlans(xs)
-    case "--server"   :: xs => CourseWebServer.start(xs); // main(args.tail)
-    case xs                 => println(Statistics(xs))
+
+    case "--server"   :: xs =>
+      CourseWebServer.start(
+        host = xs.lift(0).getOrElse("0.0.0.0"),
+        port = scala.util.Try(xs(1).toInt).getOrElse(8090)
+      )
+
+    case xs  => println(Statistics(xs))
   }
 }
